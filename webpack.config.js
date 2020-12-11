219c421b17e9ee173b98288431eb17b4c9b82d42
Oksana Tkachenko <coatceatl@gmail.com>
2020-12-11 15:16:09 +0200

    adding dev-server to use live reloading(port 3300)

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,16 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[hash].js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3300,
+    open: true
   },
   plugins: [
     new HtmlWebpackPlugin(),
