b0f15d512c11fa770c0295f0516c822ea0e0ede9
Oksana Tkachenko <coatceatl@gmail.com>
2020-12-11 13:54:56 +0200

    adding plugin to cleaning the dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    filename: '[name].[hash].js',
   },
   plugins: [
     new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
 }
