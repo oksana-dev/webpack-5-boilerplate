3e115008001d40aad52e876ac947fdd51372b95a
Oksana Tkachenko <coatceatl@gmail.com>
2020-12-12 11:54:35 +0200

    adding babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './styles/index.scss'
 import Logo from './assets/img/webpack-logo.png'
 
+class Time {
+  static value = Date.now()
+}
+
+console.log('testing babel plugin ', Time.value)
 console.log('Logo:', Logo)
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const devMode = process.env.NODE_ENV === 'development'
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[hash].js',
@@ -47,6 +47,17 @@ module.exports = {
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       }
     ],
   },
